a7c78b2e5855840b3079895185637ad79c751483
Yhael Palomino <yhaelpb8@gmail.com>
2026-03-04 19:58:41 -0600

    Show pokemons on screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,24 @@
-import { Text, View } from "react-native";
-import { use, useEffect } from "react";
+import { Text, View} from "react-native";
+import { useEffect, useState } from "react";
 export default function Index() {
+  const [result, setResults] = useState<any[]>([]);
   useEffect(() => {
     console.log("entre en pantalla");
     getPokemons();
   }, []);
 
+
   const getPokemons = async () => {
-    const url = "https://pokeapi.co/api/v2/pokemon/1/";
+    const url = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
     const response = await fetch(url); /*request a la api */
     const data = await response.json();
-    console.log(data);
+    setResults(data.results);
   };
   return (
     <View>
-      <Text>Yhael</Text>
+      {result.map((item) => {
+        return <Text key={item.name}>{item.name}</Text>
+      })}
     </View>
   );
 }
